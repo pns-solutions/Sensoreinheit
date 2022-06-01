14547b6a9ac9668493954254c5fff6dc17076ffd
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2022-06-01 17:35:18 +0200

    Update mqtt.ino

--- mqtt/mqtt.ino
+++ mqtt/mqtt.ino
@@ -19,8 +19,8 @@
 
 #include "arduino_secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = "Martin Router King";    // your network SSID (name)
-char pass[] = "DasFest2018!";    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "";    // your network SSID (name)
+char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
 
 // To connect with SSL/TLS:
 // 1) Change WiFiClient to WiFiSSLClient.
